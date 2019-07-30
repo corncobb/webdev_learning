16343af4741f1f0141a909f8c0f14d68b7f8a8fa
Cameron Cobb <cameronjcobb@gmail.com>
2019-07-29 23:42:24 -0700

    made some new changes to todo project

--- Udemy-web-assaignment/JS-todo-project/script.js
+++ Udemy-web-assaignment/JS-todo-project/script.js
@@ -1,15 +1,24 @@
 var ans;
 var todos = [];
 
-window.setTimeout(function() {
+window.setTimeout(function(){
     while(ans != "quit"){
         ans = prompt("What would you like to do?");
         if(ans == "new"){
             ans = prompt("Enter a new todo");
             todos.push(ans);
-            console.log(todos)
+            console.log(ans + " added to list");
         } else if(ans == "list"){
-            console.log(todos);
+            console.log("**********")
+            for(var i = 0; i < todos.length; i++){
+                console.log(i + ": " + todos[i]);
+                }
+            console.log("**********");
+        } else if(ans == "delete"){
+            ans = prompt("Enter index you would like to delete");
+            var deleted = todos[ans];
+            todos.splice(ans, 1);
+            console.log("Successfully deleted " + deleted);
         }
     }
     console.log("You quit the app")
